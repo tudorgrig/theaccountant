6e2947cc80ab97b7409e3af89954fa9de967e931
Florin Iacob <florin.e.iacob@gmail.com>
2015-12-18 21:08:32 +0200

    Spring configuration to allow CORS request.

--- src/main/java/com/myMoneyTracker/app/MyMoneyTrackerApplication.java
+++ src/main/java/com/myMoneyTracker/app/MyMoneyTrackerApplication.java
@@ -2,11 +2,25 @@ package com.myMoneyTracker.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class MyMoneyTrackerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MyMoneyTrackerApplication.class, args);
+    }
+    
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:9000");
+            }
+        };
     }
 }
