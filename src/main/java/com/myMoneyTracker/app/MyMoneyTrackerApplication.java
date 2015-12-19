7412e7f7d29faac7b0ed0a3270692857fe67c81c
Florin Iacob <florin.e.iacob@gmail.com>
2015-12-19 19:43:38 +0200

    ApplicationConfig.java removed.

--- src/main/java/com/myMoneyTracker/app/MyMoneyTrackerApplication.java
+++ src/main/java/com/myMoneyTracker/app/MyMoneyTrackerApplication.java
@@ -1,12 +1,18 @@
 package com.myMoneyTracker.app;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan(basePackages = {"com.myMoneyTracker.*"})
 @SpringBootApplication
 public class MyMoneyTrackerApplication {
 
